14dbeefad9b2c07d9fd2c55f957391580fd24b3e
AbstractAlgebra <djcox@uvic.ca>
2019-02-24 12:58:03 -0800

    Snake Update

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -288,3 +288,19 @@ public class Snake {
 
 
 }
+public class TupleB
+{
+    public int x;
+    public int y;
+
+    public TupleB(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(TupleB o)
+    {
+        return (this.x == o.x && this.y == o.y);
+    }
+}
